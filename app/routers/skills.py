a606bf47527df656b729496913a0debdcb47fc90
Tuyishime Johnson <j.tuyishime4@alustudent.com>
2026-04-18 23:59:34 +0200

    change to async

--- app/routers/skills.py
+++ app/routers/skills.py
@@ -1,16 +1,25 @@
-from fastapi import APIRouter
-from app.schemas.skills import Skill
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
+from app.models import models
+from app.schemas.skills import SkillCreate, SkillResponse
 
 router = APIRouter(prefix="/skills", tags=["skills"])
 
 
-@router.get("/")
-async def get_skills(response_model=Skill):
-    return Skill(
-        category="Programming", skills=["Python", "FastAPI", "SQLAlchemy", "Docker"]
-    )
+@router.post("/", response_model=SkillResponse, status_code=201)
+async def create_skill(skill: SkillCreate, db: Session = Depends(get_db)):
+    db_skill = models.Skill(category=skill.category, skills=skill.skills)
+    db.add(db_skill)
+    db.commit()
+    db.refresh(db_skill)
+    return db_skill
 
 
-@router.post("/")
-async def create_skill(skill: str):
-    return {"message": f"Skill '{skill}' created successfully!"}
+@router.get("/", response_model=List[SkillResponse])
+async def get_skills(db: Session = Depends(get_db)):
+
+    return db.query(models.Skill).all()
